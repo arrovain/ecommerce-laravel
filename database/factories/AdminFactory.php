43fb775dd315a9f535530bb06c67432d53c7ec48
Mahmut İÇME <arrovain@gmail.com>
2024-09-02 16:37:45 +0300

    set default contents, seeding

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,12 @@ class AdminFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => 'admin',
+            'email' => 'admin@email.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password12'),
+            'remember_token' => Str::random(10),
+            'image' => 'https://cdn.pixabay.com/photo/2012/04/13/16/56/music-32859_1280.png'
         ];
     }
 }
