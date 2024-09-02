7360b9028a07dec289b470425b52ab3fc8462b2d
Mahmut İÇME <arrovain@gmail.com>
2024-09-02 16:17:48 +0300

    belongs to method for one to many relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'total',
+        'user_id',
+       
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2024_09_02_130308_create_orders_table.php
+++ database/migrations/2024_09_02_130308_create_orders_table.php
@@ -13,9 +13,12 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->decimal('total', 8, 2);
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
