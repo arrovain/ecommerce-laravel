89b6c4f933314e62493d2ab8e8b4b5577231101b
Mahmut İÇME <arrovain@gmail.com>
2024-09-02 17:28:35 +0300

    'model' key is set to App\Models\Admin::class, indicating that admin authentication will use a separate Admin model

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin/dashboard', [AdminController::class, 'index']->name('admin.index');
+Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.index');
+Route::get('/admin/login', [AdminController::class, 'login'])->name('admin.login');
+Route::post('/admin/login', [AdminController::class, 'auth'])->name('admin.auth');
+
     
 
