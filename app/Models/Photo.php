a53966a6033c1b1a1d58d3aacc9001eaf10ea21f
Mahmut İÇME <arrovain@gmail.com>
2024-09-02 16:25:11 +0300

    define a many-to-many relationship between Photo and Order and admin function defines a belongs-to relationship, indicating that each photo belongs to an admin.

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'url', 'body', 'is_free', 'price', 'admin_id'
+    ];
+
+    public function orders() {
+        return $this->belongsToMany(Order::class); 
+    }
+
+    public function admin()
+    {
+        return $this->belongsTo(Admin::class);
+    }
+
+    public function getUrlAttribute($value)
+    {
+        return asset($value);
+    }
 }
