c6f06e7dd20cf74b917363a8393c63a59dc33082
Mahmut İÇME <arrovain@gmail.com>
2024-09-04 10:54:18 +0300

    retrieve photos from the database

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Photo;
+
 
 class PhotoController extends Controller
 {
-    //
+
+    public function index()
+    {
+    
+        $photos = Photo::latest()->get();
+        return view('home')->with([
+            'photos' => $photos
+        ]); 
+    }
+
+ 
+    public function adminDashboard()
+    {
+     
+        $photos = Photo::latest()->get();
+        return view('admin.photos.index')->with([
+            'photos' => $photos
+        ]); 
+    }
+
+ 
+    public function create()
+    {
+       
+        return view('admin.photos.create');
+    }
+
 }
