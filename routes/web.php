11896bc56938930ccdbc8d2da18f2e7a61cae87a
Mahmut İÇME <arrovain@gmail.com>
2024-09-04 10:47:26 +0300

    middleware route with group function

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,7 @@ Route::get('/admin/login', [AdminController::class, 'login'])->name('admin.login
 Route::post('/admin/login', [AdminController::class, 'auth'])->name('admin.auth');
 
     
-
+Route::middleware('admin')->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.index');
+    Route::get('/admin/logout', [AdminController::class, 'logout'])->name('admin.logout');
+});
