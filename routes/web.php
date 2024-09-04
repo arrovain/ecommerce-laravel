6af5c1a04b10daf2fe513c92a685d11f1ddb40b9
Mahmut İÇME <arrovain@gmail.com>
2024-09-04 11:19:55 +0300

    create and edit pages and route for them

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\PhotoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.index');
@@ -11,4 +12,6 @@ Route::post('/admin/login', [AdminController::class, 'auth'])->name('admin.auth'
 Route::middleware('admin')->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.index');
     Route::get('/admin/logout', [AdminController::class, 'logout'])->name('admin.logout');
-});
+    Route::get('admin/photo/create', [PhotoController::class,'create'])->name('admin.photos.create');
+    Route::post('admin/photo/store', [PhotoController::class,'store'])->name('admin.photos.store');
+    Route::get('admin/{photo}/edit', [PhotoController::class,'edit'])->name('admin.photos.edit');
